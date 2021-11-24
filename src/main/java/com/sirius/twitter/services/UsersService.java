51f08fb462225a5b48745b74cbb12cd7b84d8fc7
Nicolas Cavasin <ncavasin97@gmail.com>
2021-11-24 13:51:59 -0300

    init service implementation using grpc below

--- src/main/java/com/sirius/twitter/services/UsersService.java
+++ src/main/java/com/sirius/twitter/services/UsersService.java
@@ -1,47 +1,112 @@
 package com.sirius.twitter.services;
 
-import com.sirius.twitter.UsersServiceGrpc.UsersServiceImplBase;
-import com.sirius.twitter.models.User;
-import com.sirius.twitter.repositories.IUsersRepositoryGrpc;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.sirius.twitter.*;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+
 @Service
-public class UsersService extends UsersServiceImplBase implements IUsersService{
+public class UsersService implements IUsersService {
 
-    @Autowired
-    IUsersRepositoryGrpc iUsersRepositoryGrpc;
+    private UsersServiceGrpc.UsersServiceBlockingStub stub;
+    private ManagedChannel managedChannel;
 
-    @Override
-    public Optional<User> findById(int id) {
-        return iUsersRepositoryGrpc.findById(id);
+    private void open_channel(){
+        this.managedChannel = ManagedChannelBuilder.forAddress("localhost", 50000)
+                .usePlaintext()
+                .build();
+
+        this.stub = UsersServiceGrpc.newBlockingStub(managedChannel);
+    }
+
+    private void close_channel(){
+        this.managedChannel.shutdown();
+        this.stub = null;
+    }
+
+    private UserDto populateUserDto(com.sirius.twitter.models.User user){
+        return UserDto.newBuilder()
+                .setDni(user.getDni())
+                .setName(user.getName())
+                .setEmail(user.getEmail())
+                .setActive(true)
+                .build();
     }
 
     @Override
-    public List<User> findAll() {
-        return iUsersRepositoryGrpc.findAll();
+    public com.sirius.twitter.models.User createUser(com.sirius.twitter.models.User user) {
+        this.open_channel();
+
+        // Create request class using received param
+        UserDto userDto = this.populateUserDto(user);
+
+        // Create response class & make the call
+        User response = this.stub.createUser(userDto);
+
+        // Log output
+        System.out.println(response.toString());
+        System.out.println(response.getId() + " " + response.getName() + " " +  response.getActive());
+
+        this.close_channel();
+
+        // Return dummy
+        return new com.sirius.twitter.models.User();
     }
 
     @Override
-    public User create(User user) {
-        return iUsersRepositoryGrpc.create(user);
+    public Optional<com.sirius.twitter.models.User> findById(int id) {
+        this.open_channel();
+        UserById userById = UserById.newBuilder().setId(id).build();
+        User response = this.stub.findById(userById);
+        System.out.println(response.toString());
+        this.close_channel();
+        return Optional.empty();
     }
 
     @Override
-    public Optional<User> update(int id, User user) {
-        return iUsersRepositoryGrpc.update(id, user);
+    public Optional<com.sirius.twitter.models.User> findByDni(int dni) {
+        this.open_channel();
+
+        this.close_channel();
+        return Optional.empty();
     }
 
     @Override
-    public Optional<User> deactivate(int id) {
-        return iUsersRepositoryGrpc.deactivate(id);
+    public Optional<com.sirius.twitter.models.User> findByName(String name) {
+        this.open_channel();
+
+        this.close_channel();
+        return Optional.empty();
     }
 
     @Override
-    public Optional<User> delete(int id) {
-        return iUsersRepositoryGrpc.delete(id);
+    public List<com.sirius.twitter.models.User> findAll() {
+        this.open_channel();
+        UsersList response = this.stub.findAll(Empty.newBuilder().build());
+        System.out.println("Users list: " + response.getUserList());
+        System.out.println("User count: #" + response.getUserCount());
+        System.out.println("User #0 name:" + response.getUser(0).getName());
+        this.close_channel();
+        return null;
+    }
+
+    @Override
+    public Optional<com.sirius.twitter.models.User> updateUser(int id, com.sirius.twitter.models.User user) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<com.sirius.twitter.models.User> deactivateUser(int id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<com.sirius.twitter.models.User> deleteUser(int id) {
+        return Optional.empty();
     }
 }
